68e7156d0211bc82dd1102ee6712dca1d557d1e5
Jason Beetham <beefers331@gmail.com>
2019-03-11 00:12:38 -0600

    Using a JSON instead of fetching from steamdb

--- Program.cs
+++ Program.cs
@@ -9,117 +9,125 @@ namespace ProtonWineManager
 {
     class Program
     {
-        static string plLocation = System.Environment.GetEnvironmentVariable("HOME") + "/Library.uud",dbURL = "https://steamdb.info/app/{0}/", cache = "/CachedGames.uud";
-        static List<string>  directories = new List<string>();
-        static Dictionary<string,string> idToPath = new Dictionary<string, string>(),idToName = new Dictionary<string, string>(), commandDesc = new Dictionary<string, string>();
+        static string plLocation = System.Environment.GetEnvironmentVariable("HOME") + "/Library.uud", jsonlocation = "https://api.steampowered.com/ISteamApps/GetAppList/v1/", loadedJson, jsonSubString="\t\"name\": ";
+        static List<string> directories = new List<string>();
+        static Dictionary<string, string> idToPath = new Dictionary<string, string>(), idToName = new Dictionary<string, string>(), commandDesc = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            commandDesc.Add("-h","Displays all commands and their actions");
-            commandDesc.Add("-c","Clears cached games");
-            commandDesc.Add("-q","Quits application");
-
-
-            cache = AppDomain.CurrentDomain.BaseDirectory + cache;
-            System.Environment.SetEnvironmentVariable("PWD",AppDomain.CurrentDomain.BaseDirectory);
-            if(File.Exists(plLocation)){
-            using (StreamReader sr = new StreamReader(File.OpenRead(plLocation)))
+            commandDesc.Add("-h", "Displays all commands and their actions");
+            commandDesc.Add("-c", "Clears Gamesjson");
+            commandDesc.Add("-q", "Quits application");
+            string localJsonPath = AppDomain.CurrentDomain.BaseDirectory + "/Games.json";
+            System.Environment.SetEnvironmentVariable("PWD", AppDomain.CurrentDomain.BaseDirectory);
+            if (File.Exists(plLocation))
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = new StreamReader(File.OpenRead(plLocation)))
                 {
-                    directories.Add(sr.ReadLine().Trim());
-                }
-            }
-            if(File.Exists(cache)){
-                using(StreamReader sr = new StreamReader(File.OpenRead(cache))){
-                    while(!sr.EndOfStream){
-                        string[] data = sr.ReadLine().Split("^");
-                        if(Directory.Exists(data[2])){
-                            idToName.Add(data[0],data[1]);
-                            idToPath.Add(data[0],data[2]);
-                        }
+                    while (!sr.EndOfStream)
+                    {
+                        directories.Add(sr.ReadLine().Trim());
                     }
                 }
-            }
             FetchGames:;
-            string gameID = "";
-            foreach (string dir in directories)
-            {
-                string properDir = dir + "/steamapps/compatdata/";
-                foreach (string subDir in Directory.GetDirectories(properDir))
+                if (!File.Exists(localJsonPath))
                 {
-                    gameID = subDir.Substring(properDir.Length);
-                    if(gameID != "pfx" && !idToName.Keys.Contains(gameID)){
-                        idToPath.Add(gameID,subDir + "/pfx");
-                        using(StreamReader sr = new StreamReader(WebRequest.Create(string.Format(dbURL,gameID)).GetResponse().GetResponseStream())){
-                            for(int i = 0 ;i <5;i++){
-                                sr.ReadLine();
-                            }
-                            idToName.Add(gameID,sr.ReadLine().Split("title>")[1].Split('·')[0]);
+                    loadedJson = new StreamReader(WebRequest.Create(jsonlocation).GetResponse().GetResponseStream()).ReadToEnd();
+                    using (StreamWriter sw = new StreamWriter(File.Create(localJsonPath)))
+                    {
+                        sw.Write(loadedJson);
+                    }
+                }
+                else
+                {
+                    using (StreamReader sr = new StreamReader(File.OpenRead(localJsonPath)))
+                    {
+                        loadedJson = sr.ReadToEnd();
+                    }
+                }
+                string gameID = "";
+                foreach (string dir in directories)
+                {
+                    string properDir = dir + "/steamapps/compatdata/";
+                    foreach (string subDir in Directory.GetDirectories(properDir))
+                    {
+                        gameID = subDir.Substring(properDir.Length);
+                        if (gameID != "pfx" && !idToName.Keys.Contains(gameID))
+                        {
+                            idToPath.Add(gameID, subDir + "/pfx");
+                            string temp = loadedJson.Split(string.Format(" {0},",gameID))[1].Split("\n")[1];
+                            temp = temp.Replace(jsonSubString,"").Replace("\"","");
+                            idToName.Add(gameID, temp);
                         }
                     }
                 }
-            }
-            using(StreamWriter sw = new StreamWriter(File.OpenWrite(cache))){
-                string[] ids = idToName.Keys.ToArray();
-                for(int i =0; i < ids.Length;i++){
-                    sw.WriteLine(string.Format("{0}^{1}^{2}",ids[i],idToName[ids[i]],idToPath[ids[i]]));
-                }
-            }
-            string[] keys = idToName.Keys.ToArray();
+                string[] keys = idToName.Keys.ToArray();
             ProtonWineManager:;
-            WriteColoredConsole("Type a command after the number you want to use",ConsoleColor.Green);
-            for(int i =0;i < keys.Length;i++){
-                WriteColoredConsole(string.Format("[{0}] : {1} / APP ID : {2}",i,idToName[keys[i]],keys[i]),ConsoleColor.Yellow);
-            }
-            string response = Console.ReadLine();
-            string[] commandKeys = commandDesc.Keys.ToArray();
-            if(commandKeys.Contains(response)){
-                if(response.Equals("-h")){
-                    for(int i =0; i < commandKeys.Length;i++){
-                        WriteColoredConsole(commandKeys[i] + "\t" + commandDesc[commandKeys[i]],ConsoleColor.White);
+                WriteColoredConsole("Type a command after the number you want to use", ConsoleColor.Green);
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    WriteColoredConsole(string.Format("[{0}] : {1} / APP ID : {2}", i, idToName[keys[i]], keys[i]), ConsoleColor.Yellow);
+                }
+                string response = Console.ReadLine();
+                string[] commandKeys = commandDesc.Keys.ToArray();
+                if (commandKeys.Contains(response))
+                {
+                    if (response.Equals("-h"))
+                    {
+                        for (int i = 0; i < commandKeys.Length; i++)
+                        {
+                            WriteColoredConsole(commandKeys[i] + "\t" + commandDesc[commandKeys[i]], ConsoleColor.White);
+                        }
+                        goto ProtonWineManager;
                     }
+                    if (response.Equals("-c"))
+                    {
+                        File.Delete(localJsonPath);
+                        idToName.Clear();
+                        idToPath.Clear();
+                        goto FetchGames;
+                    }
+                    if (response.Equals("-q"))
+                    {
+                        return;
+                    }
+                }
+                if (!response.Contains(" "))
+                {
+                    goto CommandError;
+                }
+                try
+                {
+                    string[] splitResponse = response.Split(" ");
+                    WriteColoredConsole(string.Format("{1} Selected at : {0}", idToPath[keys[int.Parse(splitResponse[0])]], idToName[keys[int.Parse(splitResponse[0])]]), ConsoleColor.Blue);
+                    Process process = new Process();
+                    process.StartInfo.FileName = splitResponse[1];
+                    process.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                    for (int i = 2; i < splitResponse.Length; i++)
+                    {
+                        process.StartInfo.ArgumentList.Add(splitResponse[i]);
+                    }
+                    process.StartInfo.EnvironmentVariables.Add("WINEPREFIX", idToPath[keys[int.Parse(splitResponse[0])]]);
+                    process.Start();
                     goto ProtonWineManager;
                 }
-                if(response.Equals("-c")){
-                    File.Delete(cache);
-                    idToName.Clear();
-                    idToPath.Clear();
-                    goto FetchGames;
+                catch (Exception e)
+                {
+                    WriteColoredConsole(e.ToString(), ConsoleColor.Magenta);
+                    goto CommandError;
                 }
-                if(response.Equals("-q")){
-                    return;
-                }
-            }
-            if(!response.Contains(" ")){
-                goto CommandError;
-            }
-            try{
-                string[] splitResponse = response.Split(" ");
-                WriteColoredConsole(string.Format("{1} Selected at : {0}",idToPath[keys[int.Parse(splitResponse[0])]],idToName[keys[int.Parse(splitResponse[0])]]),ConsoleColor.Blue);
-                Process process = new Process();
-                process.StartInfo.FileName = splitResponse[1];
-                process.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
-                for(int i =2; i < splitResponse.Length;i++){
-                    process.StartInfo.ArgumentList.Add(splitResponse[i]);
-                }
-                process.StartInfo.EnvironmentVariables.Add("WINEPREFIX",idToPath[keys[int.Parse(splitResponse[0])]]);
-                process.Start();
-                goto ProtonWineManager;
-            }catch(Exception e){
-                WriteColoredConsole(e.ToString(),ConsoleColor.Magenta);
-                goto CommandError;
-            }
             CommandError:;
-                WriteColoredConsole("Please check the command and try again.",ConsoleColor.Red);
+                WriteColoredConsole("Please check the command and try again.", ConsoleColor.Red);
                 Console.ReadKey();
-            goto ProtonWineManager;
+                goto ProtonWineManager;
             }
-            else{
-                WriteColoredConsole(plLocation + " not found.",ConsoleColor.Red);
+            else
+            {
+                WriteColoredConsole(plLocation + " not found.", ConsoleColor.Red);
                 Console.ReadKey();
             }
         }
-        static void WriteColoredConsole(string content, ConsoleColor color){
+        static void WriteColoredConsole(string content, ConsoleColor color)
+        {
             ConsoleColor temp = Console.ForegroundColor;
             Console.ForegroundColor = color;
             Console.WriteLine(content);
